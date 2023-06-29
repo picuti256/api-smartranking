15ecc3d32e320fff9b34622b206b6d2fd34e03a6
Giovanne Piçuti <picuti256@gmail.com>
2023-06-28 21:14:48 -0300

    Criado metodo put e get

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -1,13 +1,17 @@
 import {
   Body,
   Controller,
+  Get,
+  Param,
   Post,
+  Put,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { Categoria } from './interface/categoria.interface';
 import { CategoriasService } from './categorias.service';
 import { CriarCategoriaDto } from './dtos/criar-cateogoria.dto';
+import { AtualizarCategoriaDto } from './dtos/atualizar-categoria.dto';
 
 @Controller('api/v1/categorias')
 export class CategoriasController {
@@ -19,5 +23,28 @@ export class CategoriasController {
     @Body() criarCategoriaDto: CriarCategoriaDto,
   ): Promise<Categoria> {
     return await this.categoriaService.criarCategoria(criarCategoriaDto);
+  }
+
+  @Get()
+  async consultarCategorias(): Promise<Array<Categoria>> {
+    return await this.categoriaService.consultarTodasCategorias();
+  }
+
+  @Get('/:categoria')
+  async consultCategoriaPeloId(
+    @Param('categoria') categoria: string,
+  ): Promise<Categoria> {
+    return await this.categoriaService.consultarCategoriaPeloId(categoria);
+  }
+
+  @Put('/:categoria')
+  async atualizaCategoria(
+    @Body() atualizarCategoriaDto: AtualizarCategoriaDto,
+    @Param('categoria') categoria: string,
+  ): Promise<void> {
+    return await this.categoriaService.atualizaCategoria(
+      categoria,
+      atualizarCategoriaDto,
+    );
   }
 }
--- src/categorias/categorias.service.ts
+++ src/categorias/categorias.service.ts
@@ -1,8 +1,13 @@
-import { BadGatewayException, Injectable } from '@nestjs/common';
+import {
+  BadGatewayException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Categoria } from './interface/categoria.interface';
 import { CriarCategoriaDto } from './dtos/criar-cateogoria.dto';
+import { AtualizarCategoriaDto } from './dtos/atualizar-categoria.dto';
 
 @Injectable()
 export class CategoriasService {
@@ -27,5 +32,43 @@ export class CategoriasService {
 
     const categoriaCriada = new this.categoriaModel(criarCategoriaDto);
     return await categoriaCriada.save();
+  }
+
+  async consultarTodasCategorias(): Promise<Array<Categoria>> {
+    return await this.categoriaModel.find().exec();
+  }
+
+  async consultarCategoriaPeloId(categoria: string): Promise<Categoria> {
+    const categoriaEncontrada = await this.categoriaModel
+      .findOne({
+        categoria,
+      })
+      .exec();
+
+    if (!categoriaEncontrada) {
+      throw new NotFoundException(
+        `A categoria ${categoria} informada não foi encontrada.`,
+      );
+    }
+    return categoriaEncontrada;
+  }
+
+  async atualizaCategoria(
+    categoria: string,
+    atualizarCategoria: AtualizarCategoriaDto,
+  ): Promise<void> {
+    const categoriaEncontrada = await this.categoriaModel
+      .findOne({
+        categoria,
+      })
+      .exec();
+
+    if (!categoriaEncontrada) {
+      throw new NotFoundException(`Categoria ${categoria} não encontrada.`);
+    }
+
+    await this.categoriaModel
+      .findOneAndUpdate({ categoria }, { $set: atualizarCategoria })
+      .exec();
   }
 }
